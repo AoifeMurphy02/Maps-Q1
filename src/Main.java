e5469450210545e80a1f197d9b0e84fd79c479a7
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2023-12-07 22:14:57 +0000

    oop map question 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.sql.SQLOutput;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +17,9 @@ public class Main {
         countyCodeMap.put("C", "Cork");
         countyCodeMap.put("MN","Monaghan");
         countyCodeMap.put("KK","Kilkenny");
+
+        displayMap(countyCodeMap);
+
         //c) Write a loop that repeatedly asks the user for a county code, and displays the county
         //name if found, and otherwise, displays “County code not found”. [ use get(key) ]
         Scanner scanner = new Scanner(System.in);
@@ -37,10 +42,30 @@ public class Main {
                 System.out.println("County code not found");
             }
         }
-        scanner.close();
         //d) Write a loop that repeatedly asks the user for a county name, and returns “Found”
         //or “Not Found” depending on whether the county is in the map or not.
+        boolean found = false;
+        do{
+            System.out.println("Enter a country name: ");
+            String in = scanner.nextLine();
+            Boolean county = countyCodeMap.containsValue(in);
+            if(county){
+
+                System.out.println("found");
+                found = true;
+            }
+            else{
+                System.out.println("Not Found");
+            }
+        }while (!found);
+
         //e) Write a function displayMap() that accepts the map as an argument and uses a loop
         //to display all the mappings from the map, showing both key and value.
     }
+    public static void displayMap(Map<String, String> map){
+        for(int i =0; i < map.size(); i++){
+            System.out.println(map.entrySet());
+        }
+    }
+
 }
